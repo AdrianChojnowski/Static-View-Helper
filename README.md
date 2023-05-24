fb6106fb9e415fb6d391a926c0f1cd0a13dcf8fd
AdrianChojnowski <132752943+AdrianChojnowski@users.noreply.github.com>
2023-05-24 20:13:58 +0200

    Program is finished :)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Static-View-Helper
+
+Simple Python program to help automate naming files at work
---
+++ Static-View-Helper.py
@@ -0,0 +1,38 @@
+import os
+import glob
+import sys
+import pathlib
+
+def main():
+    path = pathlib.Path(sys.argv[1])
+    for folder in path.iterdir():
+        if folder.is_dir():
+            counter = 1
+            for file in folder.iterdir():
+                            if file.is_file():
+                                x = view(counter)
+                                y = DayOrNight(counter)
+                                new_file = f"City{folder.name}-View{x}-{y}{file.suffix}"
+                                file.rename(path / folder.name / new_file)
+                                counter += 1
+
+
+def view(x):
+      if x == 1 or x == 2:
+            return "1"
+      elif x == 3 or x == 4:
+            return "2"
+      elif x == 5 or x == 6:
+            return "3"
+      else:
+            return "4"
+      
+def DayOrNight(x):
+      if (x%2) == 0:
+            return "Night"
+      else:
+            return "Day"
+
+
+if __name__ == "__main__":
+    main()
